b80bd3a3ad386f9c533c596b1230a32eac79c765
Foxy20122012 <malvarezm26@miumg.edu.gt>
2023-11-11 20:50:03 -0600

    pagina de precios

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
     public function index() : View
     {
-        $products = Product::paginate(9);
+        $products = Product::paginate(6);
         return view('products.index', compact('products'));
     }
 }
